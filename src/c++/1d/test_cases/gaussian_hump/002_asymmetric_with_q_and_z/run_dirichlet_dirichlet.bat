40258015e7377a8cd9274b011e12aca6d9621edb
Jan Mooiman <jan.mooiman@outlook.com>
2025-03-17 15:48:37 +0100

    Create the non-standard output directory if it does not exists

--- src/c++/1d/test_cases/gaussian_hump/002_asymmetric_with_q_and_z/run_dirichlet_dirichlet.bat
+++ src/c++/1d/test_cases/gaussian_hump/002_asymmetric_with_q_and_z/run_dirichlet_dirichlet.bat
@@ -3,4 +3,5 @@ set exe_dir=..\..\..\1d_wave\bin\x64\Release
 del /q .\output\*.*
 %exe_dir%\1d_wave.exe --toml .\input_cpp_dirichlet_dirichlet.toml
 del /q .\output_dirichlet_dirichlet\*.*
+mkdir .\output_dirichlet_dirichlet
 xcopy /s /y output output_dirichlet_dirichlet
