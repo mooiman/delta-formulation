064bdafb715c5d4122adcdb38d1d392fa9f67044
Jan Mooiman <jan.mooiman@outlook.com>
2025-03-30 22:34:24 +0200

    Update of input files and scripts for several test cases

--- src/c++/1d/test_cases/wavy_bedlevel/002_temporal/run_tests.bat
+++ src/c++/1d/test_cases/wavy_bedlevel/002_temporal/run_tests.bat
@@ -1,25 +1,25 @@
 set exe_dir=..\..\..\1d_wave\bin\x64\Release
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=01d25.toml
-del /q ".\output_dx=01d25"
-mkdir ".\output_dx=01d25"
-xcopy /s /y output output_dx=01d25
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx01d25.toml
+del /q ".\output_dx01d25"
+mkdir ".\output_dx01d25"
+xcopy /s /y output output_dx01d25
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=02d50.toml
-del /q ".\output_dx=02d50"
-mkdir ".\output_dx=02d50"
-xcopy /s /y output output_dx=02d50
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx02d50.toml
+del /q ".\output_dx02d50"
+mkdir ".\output_dx02d50"
+xcopy /s /y output output_dx02d50
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=05d00.toml
-del /q ".\output_dx=05d00"
-mkdir ".\output_dx=05d00"
-xcopy /s /y output output_dx=05d00
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx05d00.toml
+del /q ".\output_dx05d00"
+mkdir ".\output_dx05d00"
+xcopy /s /y output output_dx05d00
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=10d00.toml
-del /q ".\output_dx=10d00"
-mkdir ".\output_dx=10d00"
-xcopy /s /y output output_dx=10d00
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx10d00.toml
+del /q ".\output_dx10d00"
+mkdir ".\output_dx10d00"
+xcopy /s /y output output_dx10d00
--- src/c++/1d/test_cases/wavy_sloped_bedlevel/001_stationary/run_tests.bat
+++ src/c++/1d/test_cases/wavy_sloped_bedlevel/001_stationary/run_tests.bat
@@ -1,31 +1,31 @@
 set exe_dir=..\..\..\1d_wave\bin\x64\Release
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=00d625.toml
-del /q ".\output_dx=00d625"
-mkdir ".\output_dx=00d625"
-xcopy /s /y output output_dx=00d625
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx00d625.toml
+del /q ".\output_dx00d625"
+mkdir ".\output_dx00d625"
+xcopy /s /y output output_dx00d625
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=01d25.toml
-del /q ".\output_dx=01d25"
-mkdir ".\output_dx=01d25"
-xcopy /s /y output output_dx=01d25
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx01d25.toml
+del /q ".\output_dx01d25"
+mkdir ".\output_dx01d25"
+xcopy /s /y output output_dx01d25
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=02d50.toml
-del /q ".\output_dx=02d50"
-mkdir ".\output_dx=02d50"
-xcopy /s /y output output_dx=02d50
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx02d50.toml
+del /q ".\output_dx02d50"
+mkdir ".\output_dx02d50"
+xcopy /s /y output output_dx02d50
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=05d00.toml
-del /q ".\output_dx=05d00"
-mkdir ".\output_dx=05d00"
-xcopy /s /y output output_dx=05d00
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx05d00.toml
+del /q ".\output_dx05d00"
+mkdir ".\output_dx05d00"
+xcopy /s /y output output_dx05d00
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_cpp_dx=10d00.toml
-del /q ".\output_dx=10d00"
-mkdir ".\output_dx=10d00"
-xcopy /s /y output output_dx=10d00
+%exe_dir%\1d_wave.exe --toml .\input_cpp_dx10d00.toml
+del /q ".\output_dx10d00"
+mkdir ".\output_dx10d00"
+xcopy /s /y output output_dx10d00
