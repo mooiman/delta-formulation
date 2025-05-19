f8a9b18e065132093cb1d54417b10af585be31b7
Jan Mooiman <jan.mooiman@outlook.com>
2025-05-19 21:55:07 +0200

    1D advection-diffusion test cases update

---
+++ src/c++/1d/test_cases/e02_1d_advection_diffusion/f09_advection_diffusion_interface_stationary/run_tests.bat
@@ -0,0 +1,31 @@
+cd c01_u0d0_dx0d001
+call run_test.bat
+cd ..
+
+cd c02_u0d0_dx0d01
+call run_test.bat
+cd ..
+
+cd c03_u0d0_dx0d05
+call run_test.bat
+cd ..
+
+cd c04_u0d0_dx0d10
+call run_test.bat
+cd ..
+
+cd c05_u0d9_dx0d001
+call run_test.bat
+cd ..
+
+cd c06_u0d9_dx0d01
+call run_test.bat
+cd ..
+
+cd c07_u0d9_dx0d05
+call run_test.bat
+cd ..
+
+cd c08_u0d9_dx0d10
+call run_test.bat
+cd ..
