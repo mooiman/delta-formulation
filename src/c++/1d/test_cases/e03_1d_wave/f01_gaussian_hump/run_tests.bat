3a34da3c613082a1b2fa19763579c7e4eb9c8cc5
Jan Mooiman <jan.mooiman@outlook.com>
2025-05-19 21:34:12 +0200

    1D wave test cases reorganizated

---
+++ src/c++/1d/test_cases/e03_1d_wave/f01_gaussian_hump/run_tests.bat
@@ -0,0 +1,23 @@
+cd c01_symmetric_bb_qq
+call run_tests.bat
+cd ..
+
+cd c02_symmetric_bb_qz
+call run_tests.bat
+cd ..
+
+cd c03_symmetric_bb_zq
+call run_tests.bat
+cd ..
+
+cd c04_symmetric_bb_zz
+call run_tests.bat
+cd ..
+
+cd c05_symmetric_mm_qz
+call run_tests.bat
+cd ..
+
+cd c06_convection
+call run_tests.bat
+cd ..
