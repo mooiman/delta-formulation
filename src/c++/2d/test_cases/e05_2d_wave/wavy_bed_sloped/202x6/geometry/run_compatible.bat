e45378d9e696a85e79241469b7fa57dd137a0ba8
Jan Mooiman <jan.mooiman@outlook.com>
2025-12-18 21:28:46 +0100

    Enable the initial regularization of the bed level for the test case with a 'horizontal' wavy bed

--- src/c++/2d/test_cases/e05_2d_wave/wavy_bed_sloped/202x6/geometry/run_compatible.bat
+++ src/c++/2d/test_cases/e05_2d_wave/wavy_bed_sloped/202x6/geometry/run_compatible.bat
@@ -1,4 +1,1 @@
-c:\bin\bed_level_not_compatible.exe --rgf 102x6_dx10d00.grd --bed-type wavy_sloped
-c:\bin\bed_level_not_compatible.exe --rgf 202x6_dx05d00.grd --bed-type wavy_sloped
-c:\bin\bed_level_not_compatible.exe --rgf 402x6_dx02d50.grd --bed-type wavy_sloped
-c:\bin\bed_level_not_compatible.exe --rgf 802x6_dx01d25.grd --bed-type wavy_sloped
+c:\bin\bed_level_compatible.exe --rgf 202x6_dx05d00.grd --bed-type wavy_sloped
