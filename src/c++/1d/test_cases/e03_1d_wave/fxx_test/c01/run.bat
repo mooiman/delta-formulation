657639e4949b28d62c62ae35b92df595dbb9edeb
Jan Mooiman <jan.mooiman@outlook.com>
2026-03-14 21:05:16 +0100

    1D wave update test cases

--- src/c++/1d/test_cases/e03_1d_wave/fxx_test/c01/run.bat
+++ src/c++/1d/test_cases/e03_1d_wave/fxx_test/c01/run.bat
@@ -1,8 +1,8 @@
-set exe_dir=..\..\..\..\1d_wave\bin\x64\Release
+set exe_dir=..\..\..\..\wave_1d\bin\x64\Release
 
 del /q .\output_01\*.*
 mkdir .\output_01
 xcopy /s /y .\output  .\output_01
 
 del /q .\output\*.*
-%exe_dir%\1d_wave.exe --toml .\input_1d.toml
+%exe_dir%\wave_1d.exe --toml .\input_cpp.toml
