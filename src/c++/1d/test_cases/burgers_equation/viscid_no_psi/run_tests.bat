89ed79cbe9122c1b54256e3fd865b57b958936f2
Jan Mooiman <jan.mooiman@outlook.com>
2026-07-30 23:21:49 +0200

    Test cases added for the Burgers equation, these test cases do not add artificial viscoisty

---
+++ src/c++/1d/test_cases/burgers_equation/viscid_no_psi/run_tests.bat
@@ -0,0 +1,27 @@
+rem cd dx01_dt01_nu00d1
+rem call run.bat
+rem cd ..
+
+cd dx05_dt05_nu00d1
+call run.bat
+cd ..
+
+cd dx05_dt05_nu10d0
+call run.bat
+cd ..
+
+cd dx10_dt05_nu00d1
+call run.bat
+cd ..
+
+cd dx10_dt05_nu10d0
+call run.bat
+cd ..
+
+cd dx10_dt10_nu00d1
+call run.bat
+cd ..
+
+cd dx10_dt10_nu10d0
+call run.bat
+cd ..
