ed939f616dc8dbee6258bc26986ccb613b95c3f4
Jan Mooiman <jan.mooiman@outlook.com>
2026-07-30 22:41:34 +0200

    Test cases added for the 1D advection-diffusion equation

---
+++ src/c++/1d/test_cases/advection_diffusion_1d/f01_advection_diffusion_constant_temporal/run_tests.bat
@@ -0,0 +1,19 @@
+cd c01_dx100d0
+call run_test.bat
+cd ..
+
+cd c02_dx050d0
+call run_test.bat
+cd ..
+
+cd c03_dx025d0
+call run_test.bat
+cd ..
+
+cd c04_dx010d0
+call run_test.bat
+cd ..
+
+cd c05_dx005d0
+call run_test.bat
+cd ..
