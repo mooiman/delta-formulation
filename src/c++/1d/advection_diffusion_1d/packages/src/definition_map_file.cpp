997e135a7b1002c2f4a6b9cd9706db6e0f476b4e
Jan Mooiman <jan.mooiman@outlook.com>
2026-07-30 23:28:08 +0200

    File is now UTF-8 compliant

--- src/c++/1d/advection_diffusion_1d/packages/src/definition_map_file.cpp
+++ src/c++/1d/advection_diffusion_1d/packages/src/definition_map_file.cpp
@@ -24,7 +24,6 @@
 
 UGRID1D * create_map_file(std::string nc_mapfile, std::string model_title, std::vector<double> & x, std::vector<std::string>& map_names)
 {
-
     int status = -1;
     int nr_nodes = (int)x.size();
     int nr_edges = nr_nodes - 1;
@@ -135,7 +134,7 @@ UGRID1D * create_map_file(std::string nc_mapfile, std::string model_title, std::
     status = map_file->add_variable(map_names[2], dim_names, "-", "Viscosity (used)", "m2 s-1", "mesh1D", "node", "Viscosity, including the artificial viscosity Psi");
     status = map_file->add_variable(map_names[3], dim_names, "-", "Psi", "m2 s-1", "mesh1D", "node", "Artificial viscosity");
     status = map_file->add_variable(map_names[4], dim_names, "-", "Peclet (u.dx/nu)", "-", "mesh1D", "node", "Cell Peclet number (u.dx/nu)");
-    status = map_file->add_variable(map_names[5], dim_names, "-", "CFL (u.dt/dx)", "-", "mesh1D", "node", "Courant–Friedrichs–Lewy number (u.dt/dx)");
+    status = map_file->add_variable(map_names[5], dim_names, "-", "CFL (u.dt/dx)", "-", "mesh1D", "node", "Courantâ€“Friedrichsâ€“Lewy number (u.dt/dx)");
     status = map_file->add_variable(map_names[6], dim_names, "-", "Fourier number (nu.dt/dx^2)", "-", "mesh1D", "node", "Fourier number (nu.dt/dx^2)");
 
     return map_file;
